b807a9ad943e3c6c17d4ac2a4e5a7fc6cfe5103a
Sebastian Alfieri <sebasalfieri@gmail.com>
2024-09-13 13:53:54 -0300

    avances

--- components/SimilarMovies/index.tsx
+++ components/SimilarMovies/index.tsx
@@ -1,4 +1,10 @@
+// import { useAppSelector } from "@/redux/hooks";
+
 function SimilarMovies() {
+  // const similarMovie = useAppSelector(
+  //   (state) => state.moviesStorage.mostPopular
+  // );
+
   return <div className={""}>index</div>;
 }
 
